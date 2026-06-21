1a6bec28429f44358eda99ddeb4dc61c0b67f1df
EGYx0 <a70609270@gmail.com>
2026-06-21 15:25:58 +0300

    refactor(test): use beforeEach to remove duplication

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import axios from "axios";
@@ -10,27 +10,30 @@ vi.mock("axios"); // we can mock any npm package, when we import it above and ma
 //first we have to get a mock product data to test, and also to give the product the props cause loadCart is an async function and we can't make http request cause it can modify the data
 
 // for load cart we have to mock the function, -> to create a fake version of  this function cause it's async through vi from vitest, vi creates a fake function that doesn't do anything
+
 describe("Product component", () => {
-  const product = {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  let product;
+  let loadCart;
+  beforeEach(() => {
+    product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
 
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
 
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
 
-    rating: {
-      stars: 4.5,
+      rating: {
+        stars: 4.5,
 
-      count: 87,
-    },
+        count: 87,
+      },
 
-    priceCents: 1090,
+      priceCents: 1090,
 
-    keywords: ["socks", "sports", "apparel"],
-  };
-
-  const loadCart = vi.fn();
-
+      keywords: ["socks", "sports", "apparel"],
+    };
+    loadCart = vi.fn();
+  });
   it("displays the product details correctly", () => {
     render(<Product product={product} loadCart={loadCart} />);
 
@@ -51,26 +54,6 @@ describe("Product component", () => {
     expect(screen.getByText("87")).toBeInTheDocument();
   });
   it("behaves correctly when interactions", async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-
-      rating: {
-        stars: 4.5,
-
-        count: 87,
-      },
-
-      priceCents: 1090,
-
-      keywords: ["socks", "sports", "apparel"],
-    };
-
-    const loadCart = vi.fn();
-
     render(<Product product={product} loadCart={loadCart} />);
     const addToCartButton = screen.getByTestId("add-to-cart-button");
     const user = userEvent.setup();
